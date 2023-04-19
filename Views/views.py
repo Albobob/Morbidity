ec9942cb3d862ddd0872edd97c86c1e1c0ea78b0
Albobob <treblarpip@gmail.com>
2023-04-19 11:26:57 +0300

    Добавил форму для заполнения SMP

--- Views/views.py
+++ Views/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from Morbidity.Controller.SMP import get_smp
 
@@ -10,15 +10,22 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    date_first = 2010
-    date_last = 2019
-    nz_input = 29
-    rg_id = 5
+    return render_template(f'index.html', title='Главная')
 
-    value = get_smp(date_first, date_last, nz_input, rg_id)
-    return render_template(f'index.html')
+
+@app.route('/smp', methods=['GET', 'POST'])
+def smp():
+    if request.method == 'POST':
+        date_first = int(request.form['date_first'])
+        date_last = int(request.form['date_last'])
+        nz_input = int(request.form['nz_input'])
+        rg_id = int(request.form['rg_id'])
+
+        value = get_smp(date_first, date_last, nz_input, rg_id)
+        return render_template('smp.html', value_smp=value)
+    else:
+        return render_template('smp.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
